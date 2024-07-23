87eecb33b2dce99de00bbab732a24d6599ba99b4
Muhammad Ubaid <79584441+hi-ubaid@users.noreply.github.com>
2024-07-23 17:15:25 +0500

    Second Commit with Minor Changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.listen(3010, () => {
-    console.log("Server running on port 3010");
-  });
+const db = require('./models');
+
+//Routers
+const postRouter = require('./Routes/Posts');
+
+app.use("/posts", postRouter)
+
+db.sequelize.sync().then(()=>{
+    app.listen(3010, () => {
+        console.log("Server running on port 3010");
+      });
+});
